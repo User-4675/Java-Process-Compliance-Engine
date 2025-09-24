51710d676ea5c9f9f8ce22e085cea76c17ecf7e8
samuel <samuel.mikulas10@gmail.com>
2025-09-25 00:07:34 +0200

    First attempt on Task One

--- src/main/java/nl/rug/ap/a1/Main.java
+++ src/main/java/nl/rug/ap/a1/Main.java
@@ -1,8 +1,23 @@
-package nl.rug.ap.a1; 
+package nl.rug.ap.a1;
+
+import java.io.*;
+import java.nio.charset.Charset;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Your work starts here.");
+        InputStream stream = Main.class.getClassLoader().getResourceAsStream("BPI_Challenge_2019.csv");
+        if (stream == null){
+            System.out.println("Resource file not found");
+            return;
+        }
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(stream, Charset.forName("WINDOWS-1252")))) {
+            String line;
+            while ((line = br.readLine()) != null){
+                System.out.println(line);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
